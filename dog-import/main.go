1be9b3ac918372d97508791177eb91b273351a38
Drew Gulino <dgulino@relaypro.com>
2023-01-13 15:09:35 -0500

    back to using just host name for terraform name

--- dog-import/main.go
+++ dog-import/main.go
@@ -116,7 +116,6 @@ func host_export(output_dir string, environment string) {
 
 	tf_w := terraformOutputFile(output_dir, table)
 	for _, row := range res {
-		HostKeyWithoutPlus := strings.ReplaceAll(row.HostKey, "+", "-")
 		terraformName :=  toTerraformName(row.Name) 
 		fmt.Fprintf(tf_w, "resource \"dog_host\" \"%s\" {\n", terraformName)
 		fmt.Fprintf(tf_w, "  environment = \"%s\"\n", row.Environment)
