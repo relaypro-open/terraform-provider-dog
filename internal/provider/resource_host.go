a909b4559df6c25bee5a1ae2d14e82e766824d82
Drew Gulino <1379399+dgulinobw@users.noreply.github.com>
2024-06-19 18:56:31 -0400

    added state upgrade for host, group alert_enable field

--- internal/provider/resource_host.go
+++ internal/provider/resource_host.go
@@ -390,3 +390,106 @@ func (r *hostResource) Delete(ctx context.Context, req resource.DeleteRequest, r
 	resp.Diagnostics.Append(diags...)
 	resp.State.RemoveResource(ctx)
 }
+
+type hostResourceModelV0 struct {
+	Environment string       `tfsdk:"environment"`
+	Group       string       `tfsdk:"group"`
+	ID          types.String `tfsdk:"id"`
+	HostKey     string       `tfsdk:"hostkey"`
+	Location    string       `tfsdk:"location"`
+	Name        string       `tfsdk:"name"`
+	Vars        *string      `tfsdk:"vars"`
+	AlertEnable *bool        `tfsdk:"alert_enable"`
+}
+
+type hostResourceModelV1 struct {
+	Environment string       `tfsdk:"environment"`
+	Group       string       `tfsdk:"group"`
+	ID          types.String `tfsdk:"id"`
+	HostKey     string       `tfsdk:"hostkey"`
+	Location    string       `tfsdk:"location"`
+	Name        string       `tfsdk:"name"`
+	Vars        *string      `tfsdk:"vars"`
+	AlertEnable *bool        `tfsdk:"alert_enable"`
+}
+
+func (r *hostResource) UpgradeState(ctx context.Context) map[int64]resource.StateUpgrader {
+	tflog.Debug(ctx, "UpgradeState")
+	return map[int64]resource.StateUpgrader{
+		// State upgrade implementation from 0 (prior state version) to 1 (Schema.Version)
+		0: {
+			PriorSchema: &schema.Schema{
+				// This description is used by the documentation generator and the language server.
+				MarkdownDescription: "Host data source",
+
+				Attributes: map[string]schema.Attribute{
+					// This description is used by the documentation generator and the language server.
+					"environment": schema.StringAttribute{
+						MarkdownDescription: "Host environment",
+						Optional:            true,
+					},
+					"group": schema.StringAttribute{
+						MarkdownDescription: "Host group",
+						Optional:            true,
+					},
+					"hostkey": schema.StringAttribute{
+						MarkdownDescription: "Host key",
+						Optional:            true,
+						Validators: []validator.String{
+							stringvalidator.LengthBetween(10, 256),
+							stringvalidator.RegexMatches(
+								regexp.MustCompile(`^[A-Za-z0-9+%_.-](.*)$`),
+								"must start with alphanumeric characters, %, _, ., -",
+							),
+						},
+					},
+					"location": schema.StringAttribute{
+						MarkdownDescription: "Host location",
+						Optional:            true,
+					},
+					"name": schema.StringAttribute{
+						MarkdownDescription: "Host name",
+						Optional:            true,
+					},
+					"vars": schema.StringAttribute{
+						MarkdownDescription: "json string of vars",
+						Optional:            true,
+						//Required:            true,
+					},
+					"id": schema.StringAttribute{
+						Optional:            true,
+						MarkdownDescription: "Host identifier",
+						Computed:            true,
+					},
+				},
+			},
+			StateUpgrader: func(ctx context.Context, req resource.UpgradeStateRequest, resp *resource.UpgradeStateResponse) {
+				var priorStateData hostResourceModelV0
+
+				//resp.Diagnostics.Append(
+				req.State.Get(ctx, &priorStateData)
+				//)
+
+				if resp.Diagnostics.HasError() {
+					return
+				}
+
+				var alertEnable *bool
+				req.State.GetAttribute(ctx, path.Root("alertEnable"), alertEnable)
+
+				upgradedStateData := hostResourceModelV1{
+					Environment: priorStateData.Environment,
+					Group:       priorStateData.Group,
+					HostKey:     priorStateData.HostKey,
+					Location:    priorStateData.Location,
+					Name:        priorStateData.Name,
+					Vars:        priorStateData.Vars,
+					AlertEnable: alertEnable,
+					ID:          priorStateData.ID,
+				}
+
+				resp.State.Set(ctx, upgradedStateData)
+			},
+		},
+	}
+}
